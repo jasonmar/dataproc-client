d17427cbbcee4e69e991ad90cffc9c0324cd7c1c
jessiejingxugao <gaojingxu.thu@gmail.com>
2019-05-14 10:36:34 -0700

    fall back to what worked
    ssh with "sudo su" works while using same scripts in gcloud dataproc initialization-action will fail

--- create-dproclient-vm.sh
+++ create-dproclient-vm.sh
@@ -31,13 +31,13 @@ gcloud dataproc clusters create ${YOUR_CLIENT} \
   --master-boot-disk-size 200 --image-version 1.3-deb9 \
   --tags $YOUR_CLIENT_TAG --project $YOUR_PROJECT \
   --service-account $YOUR_SERVICE_ACCOUNT \
-  --metadata=target-dataproc-cluster=${YOUR_TARGET_CLUSTER},gs-bucket-name=${YOUR_BUCKET} \
-  --initialization-actions=gs://${YOUR_BUCKET}/install-client.sh \
+  --metadata=target-dataproc-cluster=${YOUR_TARGET_CLUSTER},gs-bucket-name=${YOUR_BUCKET} 
+#  --initialization-actions=gs://${YOUR_BUCKET}/install-client.sh \
 #     --optional-components=ZEPPELIN \
  
 # . metadata.config
  
- # gcloud compute --project ${YOUR_PROJECT} ssh --zone ${YOUR_ZONE} ${YOUR_CLIENT}-m --command 'bash -s' < /$PWD/install-client.sh
+gcloud compute --project ${YOUR_PROJECT} ssh --zone ${YOUR_ZONE} ${YOUR_CLIENT}-m --command 'bash -s' < /$PWD/util/install-client.sh >> output.txt
  
  
 
--- util/install-client.sh
+++ util/install-client.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-# sudo su
+sudo su
+cd /
 # cd /
-mkdir /opt/huezep
+mkdir /opt/huezep |& tee -a /opt/huezep-output.txt
 cd /opt/huezep
 
 gsbucket="$(/usr/share/google/get_metadata_value attributes/gs-bucket-name)"
 echo $gsbucket
-gsutil cp gs://$gsbucket/metadata.config /opt/huezep/
+gsutil cp gs://$gsbucket/metadata.config /opt/huezep/ |& tee -a /opt/huezep-output.txt
 . metadata.config
 
 
@@ -20,12 +21,12 @@ gsutil cp -r gs://$gsbucket/* /opt/huezep/
 # echo $script
 
 chmod 777 startup.sh
-./startup.sh
+./startup.sh |& tee -a /opt/huezep/output.txt
 
 
 cd /usr/local/share/google/dataproc
 chmod 777 launch-agent.sh
-./launch-agent.sh
+./launch-agent.sh |& tee -a /huezep/output.txt
 
 # cd /usr/local/share/google/dataproc
 # chmod 777 startup-script-cloud_datarefinery_image_20190228_nightly-RC01.sh
@@ -33,7 +34,7 @@ chmod 777 launch-agent.sh
 
 script_image=$(grep -m 1 STARTUP_SCRIPT_LOCATION /usr/local/share/google/dataproc/launch-agent.sh | awk -F= {'print $2'})
 chmod 777 $script_image
-exec $script_image
+exec $script_image |& tee -a /opt/huezep/output.txt
 
 
 
@@ -41,12 +42,12 @@ gsutil cp gs://$gsbucket/zeppelin.sh /opt/huezep/
 gsutil cp gs://$gsbucket/hue.sh /opt/huezep/
 cd /opt/huezep
 chmod 777 hue.sh
-./hue.sh
+./hue.sh |& tee -a /opt/huezep/output.txt
 # bash -v hue.sh
 # bash zeppelin.sh
 
 chmod 777 zeppelin.sh
-./zeppelin.sh
+./zeppelin.sh |& tee -a /opt/huezep/output.txt
 
 . metadata.config
 
@@ -54,7 +55,7 @@ chmod 777 zeppelin.sh
 sudo perl -pi -e s/hive_server_host=${YOUR_CLIENT}-m.c.${YOUR_PROJECT}.internal/hive_server_host=${YOUR_TARGET_CLUSTER}-m.c.${YOUR_PROJECT}.internal/ /etc/hue/conf/hue.ini
 sudo perl -pi -e 's/## hive_server_port=10000/hive_server_port=10000/g' /etc/hue/conf/hue.ini
 
-systemctl restart hue
+systemctl restart hue 
 
 logout
 exit
