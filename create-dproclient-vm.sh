f233a446855b6f01bf0a5ef5ca987a6a324bd4dc
Jessie Gao <gaojingxu.thu@gmail.com>
2019-05-14 09:55:11 -0700

    Update create-dproclient-vm.sh

--- create-dproclient-vm.sh
+++ create-dproclient-vm.sh
@@ -37,7 +37,7 @@ gcloud dataproc clusters create ${YOUR_CLIENT} \
  
 # . metadata.config
  
- # gcloud compute --project ${YOUR_PROJECT} ssh --zone ${YOUR_ZONE} ${YOUR_CLIENT}-m --command 'bash -s' < /$PWD/install-client.sh
+ # gcloud compute --project ${YOUR_PROJECT} ssh --zone ${YOUR_ZONE} ${YOUR_CLIENT}-m --command 'bash -s' < /$PWD/util/install-client.sh
  
  
 
