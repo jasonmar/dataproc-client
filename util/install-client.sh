6580137ca480fde1692e40f953441d86dc8ac30a
jessiejingxugao <gaojingxu.thu@gmail.com>
2019-05-15 14:52:22 -0700

    sudo su inline & sudo tee

--- util/install-client.sh
+++ util/install-client.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
+
 #sudo su
 #cd /
 # cd /
-sudo mkdir /opt/huezep |& tee -a /opt/huezep-output.txt
+
+
+sudo mkdir /opt/huezep 
 cd /opt/huezep
 
 gsbucket="$(/usr/share/google/get_metadata_value attributes/gs-bucket-name)"
 echo $gsbucket
-sudo gsutil cp gs://$gsbucket/metadata.config /opt/huezep/ |& tee -a /opt/huezep-output.txt
+sudo gsutil cp gs://$gsbucket/metadata.config /opt/huezep/  |& sudo tee -a /opt/huezep/output.txt
 . metadata.config
 
 
-sudo gsutil cp -r gs://$gsbucket/* /opt/huezep/
+sudo gsutil cp -r gs://$gsbucket/* /opt/huezep/ |& sudo tee -a /opt/huezep/output.txt
 
 # target="$(/usr/share/google/get_metadata_value attributes/target-dataproc-cluster)-m"
 # echo $target
@@ -20,34 +23,40 @@ sudo gsutil cp -r gs://$gsbucket/* /opt/huezep/
 # sed -i -e "s|^MASTER_HOSTNAMES=.*|MASTER_HOSTNAMES=($target)|" -e 's|export KERBEROS_ENABLED|return;export KERBEROS_ENABLED|' $script
 # echo $script
 
-sudo chmod 777 startup.sh
-sudo ./startup.sh |& tee -a /opt/huezep/output.txt
+
+# sudo bash <<EOF
+
+# sudo chmod 777 /usr/local/share/google/dataproc/bdutil/bdutil_env.sh 
+
+sudo su chmod 777 startup.sh |& sudo tee -a /opt/huezep/output.txt
+sudo su  ./startup.sh |& sudo tee -a /opt/huezep/output.txt 
 
 
 cd /usr/local/share/google/dataproc
-sudo chmod 777 launch-agent.sh
-sudo ./launch-agent.sh |& tee -a /huezep/output.txt
+sudo su chmod 777 launch-agent.sh |& sudo tee -a /opt/huezep/output.txt
+sudo su ./launch-agent.sh |& sudo tee -a opt/huezep/output.txt
 
 # cd /usr/local/share/google/dataproc
 # chmod 777 startup-script-cloud_datarefinery_image_20190228_nightly-RC01.sh
 # ./startup-script-cloud_datarefinery_image_20190228_nightly-RC01.sh
 
 script_image=$(grep -m 1 STARTUP_SCRIPT_LOCATION /usr/local/share/google/dataproc/launch-agent.sh | awk -F= {'print $2'})
-sudo chmod 777 $script_image
-exec $script_image |& tee -a /opt/huezep/output.txt
+sudo su chmod 777 $script_image |& sudo tee -a /opt/huezep/output.txt
+sudo su exec $script_image |& sudo tee -a /opt/huezep/output.txt
 
 
 
-sudo gsutil cp gs://$gsbucket/zeppelin.sh /opt/huezep/
-sudo gsutil cp gs://$gsbucket/hue.sh /opt/huezep/
+# sudo gsutil cp gs://$gsbucket/zeppelin.sh /opt/huezep/
+#sudo gsutil cp gs://$gsbucket/hue.sh /opt/huezep/
+
 cd /opt/huezep
-sudo chmod 777 hue.sh
-sudo ./hue.sh |& tee -a /opt/huezep/output.txt
+sudo su chmod 777 hue.sh |& sudo tee -a /opt/huezep/output.txt
+sudo su ./hue.sh |& sudo tee -a /opt/huezep/output.txt
 # bash -v hue.sh
 # bash zeppelin.sh
 
-sudo chmod 777 zeppelin.sh
-sudo ./zeppelin.sh |& tee -a /opt/huezep/output.txt
+sudo su chmod 777 zeppelin.sh |& sudo tee -a /opt/huezep/output.txt
+sudo su ./zeppelin.sh |& sudo tee -a /opt/huezep/output.txt
 
 . metadata.config
 
@@ -56,6 +65,8 @@ sudo perl -pi -e s/hive_server_host=${YOUR_CLIENT}-m.c.${YOUR_PROJECT}.internal/
 sudo perl -pi -e 's/## hive_server_port=10000/hive_server_port=10000/g' /etc/hue/conf/hue.ini
 
 systemctl restart hue 
+
+# EOF
 
 logout
 exit
