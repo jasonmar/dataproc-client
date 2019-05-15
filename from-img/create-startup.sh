19901d237398439048cd8e16b9c33aca4fb87144
Jessie Gao <gaojingxu.thu@gmail.com>
2019-05-15 09:37:48 -0700

    Update create-startup.sh

--- from-img/create-startup.sh
+++ from-img/create-startup.sh
@@ -1,5 +1,6 @@
 #define parameters which are passed in
 YOUR_TARGET_CLUSTER=$1
+YOUR_PROJECT=$2
 
 #define the template
 cat  << EOF
@@ -11,5 +12,7 @@ sudo sed -i "s/hive-server-m/${YOUR_TARGET_CLUSTER}-m/g" /etc/hive/conf/hive-sit
 sudo sed -i "s/test-client-opt-init-4-structured-m/${YOUR_TARGET_CLUSTER}-m/g" /etc/spark/conf/spark-defaults.conf;
 sudo sed -i "s/hive-server-m/${YOUR_TARGET_CLUSTER}-m/g" /etc/spark/conf/spark-defaults.conf;
 /etc/spark/conf/spark-env.sh;
-sudo service spark-history-server restart
+sudo service spark-history-server restart;
+sudo perl -pi -e s/hive_server_host=hive-server-m.c.${YOUR_PROJECT}.internal/hive_server_host=${YOUR_TARGET_CLUSTER}-m.c.${YOUR_PROJECT}.internal/ /etc/hue/conf/hue.ini;
+sudo systemctl restart hue;
 EOF
