2d3ef40a2f71906f53a200f3ae560ecaa40d58ee
Jessie Gao <gaojingxu.thu@gmail.com>
2019-05-15 12:25:38 -0700

    Update create-dproclient-from-img.sh

--- from-img/create-dproclient-from-img.sh
+++ from-img/create-dproclient-from-img.sh
@@ -4,15 +4,16 @@
 
 chmod 777 create-startup.sh
 
-./create-startup.sh ${YOUR_TARGET_CLUSTER} $YOUR_PROJECT > startup-script.sh
+./create-startup.sh ${YOUR_TARGET_CLUSTER} $YOUR_PROJECT ${OLD_TARGET_CLUSTER} ${YOUR_CLIENT}> startup-script.sh
+
 
 gcloud compute --project=$YOUR_PROJECT instances create ${YOUR_CLIENT} \
- --zone=$YOUR_ZONE --machine-type=n1-standard-1 --subnet=$YOUR_NETWORK \
+ --zone=$YOUR_ZONE --machine-type=${IMG_MACHINE_TYPE} --subnet=$YOUR_NETWORK \
  --network-tier=PREMIUM --maintenance-policy=MIGRATE \
  --service-account=$YOUR_SERVICE_ACCOUNT \
  --scopes=https://www.googleapis.com/auth/cloud-platform \
  --tags=$YOUR_CLIENT_TAG --image=${YOUR_IMAGE} \
- --image-project=$YOUR_PROJECT --boot-disk-size=200GB \
- --boot-disk-type=pd-standard --boot-disk-device-name=${YOUR_CLIENT} \
+ --image-project=$YOUR_PROJECT --boot-disk-size=${IMG_BOOT_DISK_SIZE} \
+ --boot-disk-type=${IMG_BOOT_DISK_TYPE} --boot-disk-device-name=${YOUR_CLIENT} \
  --metadata-from-file startup-script=$PWD/startup-script.sh \
  --verbosity debug
