b5be8225e8ab0af50150e74b06932934e7e2000b
jessiejingxugao <gaojingxu.thu@gmail.com>
2019-05-16 22:22:34 -0700

    version 3

--- util/install-client.sh
+++ util/install-client.sh
@@ -13,6 +13,14 @@ sudo gsutil cp gs://$gsbucket/metadata.config /opt/huezep/  |& sudo tee -a /opt/
 sudo gsutil cp -r gs://$gsbucket/* /opt/huezep/ |& sudo tee -a /opt/huezep/output.txt
 
 
+
+sudo sed -i "s/${YOUR_CLIENT}-m/${YOUR_TARGET_CLUSTER}-m/g" /etc/hadoop/conf/core-site.xml /etc/hadoop/conf/hdfs-site.xml /etc/hadoop/conf/mapred-site.xml /etc/hadoop/conf/yarn-site.xml;
+sudo sed -i "s/${YOUR_CLIENT}-m/${YOUR_TARGET_CLUSTER}-m/g" /etc/hive/conf/hive-site.xml;
+sudo sed -i "s/${YOUR_CLIENT}-m/${YOUR_TARGET_CLUSTER}-m/g" /etc/spark/conf/spark-defaults.conf;
+/etc/spark/conf/spark-env.sh;
+sudo service spark-history-server restart;
+
+
 cd /opt/huezep
 sudo chmod 777 hue.sh |& sudo tee -a /opt/huezep/output.txt
 sudo ./hue.sh |& sudo tee -a /opt/huezep/output.txt
@@ -23,6 +31,8 @@ sudo ./zeppelin.sh |& sudo tee -a /opt/huezep/output.txt
 . metadata.config
 
 sudo perl -pi -e 's/## hive_server_port=10000/hive_server_port=10000/g' /etc/hue/conf/hue.ini
+sudo perl -pi -e s/hive_server_host=${YOUR_CLIENT}-m.c.${YOUR_PROJECT}.internal/hive_server_host=${YOUR_TARGET_CLUSTER}-m.c.${YOUR_PROJECT}.internal/ /etc/hue/conf/hue.ini;
+
 
 systemctl restart hue 
 
